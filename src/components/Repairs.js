007eaba54a8395f0ae78116e2d066dab9acad8c6
nicoletatum <nicoletatum@outlook.com>
2022-04-25 11:25:44 -0500

    ch1 complete

--- src/components/Repairs.js
+++ src/components/Repairs.js
@@ -1,26 +1,34 @@
 import React, { useEffect, useState } from "react"
 
 export const Repairs = () => {
-    const [customers, assignCustomers] = useState([])
+    const [customers, updatedCustomers] = useState([])
 
     useEffect(
+    //^basically an event listener
         () => {
-            fetch()
+            fetch("http://localhost:8088/customers")
                 .then(res => res.json())
-                .then(
-                    (customers) => { }
-                )
+                //set to js
+                .then((customerArray) => { 
+                    updatedCustomers(customerArray)
+                    //customers have been pulled from API and are now set in customer array
+                })
         },
-        []
+        [] 
     )
 
     return (
+        <>
         <h1>Honey Rae's Repair Shop</h1>
 
         {
             customers.map(
-                () => { }
+                (customerObject) => {
+                    return <h2>{customerObject.name}</h2>
+                    return <h2>{customerObject.address}</h2>
+                }
             )
         }
+        </>
     )
 }
